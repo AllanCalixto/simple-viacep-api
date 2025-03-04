ef69ddf2675f2b73412b1c8caf1e4edf9c856226
allancalixto <allancalixto1@gmail.com>
2025-03-03 21:55:05 -0300

    feature: first-commit

---
+++ src/main/java/br/com/simple_viacep_api/simple_viacep_api/SimpleViacepApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.simple_viacep_api.simple_viacep_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleViacepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleViacepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/simple_viacep_api/simple_viacep_api/SimpleViacepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simple_viacep_api.simple_viacep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleViacepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
